ff219bbf83f202da46cd1f28570d2626964e8ad5
mfotiadis <mfotiadis@ots.gr>
2023-09-14 09:36:48 +0300

    Deleted unused files.

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  serverUrl: '/api',
-  keycloak: {
-    issuer: 'http://localhost:8080/auth/',
-    realm: 'ThesisHub',
-    clientId: 'frontend',
-  },
-};
