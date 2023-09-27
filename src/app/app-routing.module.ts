07dc37a04a686ba80981100aae888acaaa07773a
mfotiadis <mfotiadis@ots.gr>
2023-09-27 08:17:30 +0300

    Imports and some simple paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {LandingComponent} from "./landing/landing.component";
+import {NotfoundComponent} from "./notFound/notfound.component";
+import {ThesisListComponent} from "./thesis-list/thesis-list.component";
+import {RequestedThesisComponent} from "./requested-thesis/requested-thesis.component";
 
 const routes: Routes = [
-  {path:"login", component:LoginComponent},
-  {path:"landing", component:LandingComponent},
-  {path:"", component:LandingComponent},
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  {path: "login", component: LoginComponent},
+  {path: "landing", component: LandingComponent},
+  {path: "thesis-list", component: ThesisListComponent},
+  {path: "requested-thesis", component: RequestedThesisComponent},
+  {path: "not-found", component: NotfoundComponent},
+  {path: "**", redirectTo: "/not-found"},
 ];
 
 @NgModule({
