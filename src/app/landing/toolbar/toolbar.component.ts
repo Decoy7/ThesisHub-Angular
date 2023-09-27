0ba0cdf6fcb6cf98ce5f037a701483895efb3191
mfotiadis <mfotiadis@ots.gr>
2023-09-27 08:23:07 +0300

    Toolbar component implementation of logout functionality and various redirections on clicks of buttons.

--- src/app/landing/toolbar/toolbar.component.ts
+++ src/app/landing/toolbar/toolbar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import {Component} from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
 import {Router} from "@angular/router";
 import {KeycloakService} from "../../../services/KeycloakService";
 
@@ -10,34 +10,50 @@ import {KeycloakService} from "../../../services/KeycloakService";
 })
 
 export class ToolbarComponent {
-  isLoggedIn:boolean = true;
+  isLoggedIn: boolean = true;
+  role: string = localStorage.getItem("roles")!;
 
-  constructor(private keycloakService: KeycloakService, private toast:ToastrService, private router:Router) {}
+  constructor(private keycloakService: KeycloakService, private toast: ToastrService, private router: Router) {
+  }
 
-  Logout(){
-    // @ts-ignore
-    const access_token: string = localStorage.getItem("access_token");
-    // @ts-ignore
-    const refresh_token: string  = localStorage.getItem("refresh_token");
-    // @ts-ignore
-    const username: string  = localStorage.getItem("username");
-    // @ts-ignore
-    const password: string  = localStorage.getItem("password");
+  Logout() {
+    const access_token: string = localStorage.getItem("access_token")!;
+    const refresh_token: string = localStorage.getItem("refresh_token")!;
+    const username: string = localStorage.getItem("username")!;
+    const password: string = localStorage.getItem("password")!;
 
-    if(localStorage.getItem("access_token") != null && localStorage.getItem("refresh_token") != null){
-      this.keycloakService.Logout(access_token,refresh_token,username,password).subscribe({
+    if (localStorage.getItem("access_token") != null && localStorage.getItem("refresh_token") != null) {
+      this.keycloakService.Logout(access_token, refresh_token, username, password).subscribe({
         next: () => {
           console.log("You've been logged out!");
         }
       })
-      this.toast.success('Logged out successfully!', 'Logout',{
-        progressBar:true,
-        positionClass:"toast-bottom-center",
-        closeButton:true
-      });
       localStorage.clear();
-      this.router.navigateByUrl("/login");
     }
+    this.toast.success('Logged out successfully!', 'Logout', {
+      progressBar: true,
+      positionClass: "toast-bottom-center",
+      closeButton: true
+    });
+    this.router.navigateByUrl("/login");
+  }
+
+  addUser() {
+    console.log("add user clicked");
+  }
+
+  navigateToThesisList() {
+    console.log("navigate to thesis list clicked");
+    this.router.navigateByUrl("/thesis-list");
+  }
+
+  requestedThesisAssignments() {
+    console.log("redirect requested thesis clicked");
+    this.router.navigateByUrl("/requested-thesis")
+  }
+
+  navigateBack() {
+    this.router.navigateByUrl("landing");
   }
 
 
