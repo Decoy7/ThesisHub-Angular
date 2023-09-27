af71f81a98428c311cbaf5a608334c555588d912
mfotiadis <mfotiadis@ots.gr>
2023-09-27 08:19:49 +0300

    Saving username and password in localStorage in base64 format.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {KeycloakService} from "../../services/KeycloakService";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   username?: string;
   password?: string;
 
@@ -23,11 +23,10 @@ export class LoginComponent implements OnInit {
           localStorage.setItem("refresh_token", data.refresh_token);
           localStorage.setItem("expires_in", data.expires_in);
           localStorage.setItem("refresh_expires_in", data.refresh_expires_in);
-          localStorage.setItem("session_id",data.session_state);
-          localStorage.setItem("username",<string>this.username);
-          localStorage.setItem("password",<string>this.password);
-          localStorage.setItem("roles",(JSON.parse(atob(data.access_token.split('.')[1]))).resource_access.frontend.roles);
-
+          localStorage.setItem("session_id", data.session_state);
+          localStorage.setItem("username", btoa(<string>this.username));
+          localStorage.setItem("password", btoa(<string>this.password));
+          localStorage.setItem("roles", (JSON.parse(atob(data.access_token.split('.')[1]))).resource_access.frontend.roles);
           this.toast.success('Logged in successfully!', 'Login', {
             progressBar: true,
             positionClass: "toast-bottom-center",
@@ -43,15 +42,12 @@ export class LoginComponent implements OnInit {
           });
         }
       });
-    }else{
+    } else {
       this.toast.error('Credentials are missing!', 'Error', {
         progressBar: true,
         positionClass: "toast-bottom-center",
         closeButton: true
       });
     }
-  }
-
-  ngOnInit(): void {
   }
 }
