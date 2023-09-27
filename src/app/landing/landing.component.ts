4b2b80c3afe0a284f2cd5d80c24b7a46a44c5d90
mfotiadis <mfotiadis@ots.gr>
2023-09-27 08:18:48 +0300

    Landing implementation.

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ThesisService} from "../../services/thesis.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  length!: number;
+
+  constructor(private thesisService: ThesisService, private toast: ToastrService) {
+  }
+
+  ngOnInit() {
+    this.thesisService.getAllThesis().subscribe({
+      next: data => {
+        this.length = data.length;
+      },
+      error: () => {
+        this.toast.error('Error fetching data!', 'Error', {
+          progressBar: true,
+          positionClass: "toast-bottom-center",
+          closeButton: true
+        });
+      }
+    })
+  }
 
 }
