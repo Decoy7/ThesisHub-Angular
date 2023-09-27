719d5f8fe8e8edc2b44bebbb896a8d8e93a63f9c
mfotiadis <mfotiadis@ots.gr>
2023-09-27 08:23:54 +0300

    Login and Logout service implementation.

--- src/services/KeycloakService.ts
+++ src/services/KeycloakService.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 
@@ -7,25 +7,47 @@ import {Observable} from "rxjs";
 })
 export class KeycloakService {
 
-  constructor(private http: HttpClient) {}
-
-  public Login(username: string, password: string):Observable<any>{
-    const newHeaders = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded','Host':'localhost:8080'});
-    const body = new HttpParams()
-      .set("grant_type","password")
-      .set("client_id", "frontend")
-      .set("username",username)
-      .set("password",password);
-    return this.http.post('url/realms/ThesisHub/protocol/openid-connect/token', body,{ headers: newHeaders, responseType: "json" as "json"})
+  constructor(private http: HttpClient) {
   }
 
-  public Logout(access_token: string, refresh_token: string, username: string, password: string): Observable<any>{
-    const newHeaders = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded','Host':'localhost:8080',Authorization:"Bearer "+access_token});
+  public Login(username: string, password: string): Observable<any> {
+    const newHeaders = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+    const body = new HttpParams()
+      .set("grant_type", "password")
+      .set("client_id", "frontend")
+      .set("username", username)
+      .set("password", password);
+    return this.http.post('http://localhost:8080/realms/ThesisHub/protocol/openid-connect/token', body, {
+      headers: newHeaders,
+      responseType: "json" as "json"
+    })
+  }
+
+  public Logout(access_token: string, refresh_token: string, username: string, password: string): Observable<any> {
+    const newHeaders = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: "Bearer " + access_token
+    });
     const body = new HttpParams()
       .set("client_id", "frontend")
       .set("refresh_token", refresh_token)
-      .set("username",username)
-      .set("password",password);
-    return this.http.post("url/realms/ThesisHub/protocol/openid-connect/logout",body,{headers: newHeaders, responseType: "json" as "json"});
+      .set("username", username)
+      .set("password", password);
+    return this.http.post("http://localhost:8080/realms/ThesisHub/protocol/openid-connect/logout", body, {
+      headers: newHeaders,
+      responseType: "json" as "json"
+    });
   }
+
+  public hasRole(role: string): boolean {
+    const access_token: string = localStorage.getItem("access_token")!;
+    if (access_token != undefined) {
+      const roles: string = (JSON.parse(atob(access_token.split('.')[1]))).resource_access.frontend.roles;
+      console.log(roles);
+      return roles.includes(role);
+    }
+    return false;
+  }
+
+
 }
